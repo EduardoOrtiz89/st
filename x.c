8c4eb705b6b2bf409035f58897ee6071a9f81aa6
Eduardo <olal28@gmail.com>
2020-05-05 09:36:13 -0500

    right click paster

--- x.c
+++ x.c
@@ -691,9 +691,9 @@ brelease(XEvent *e)
 		return;
 	}
 
-	if (mouseaction(e, 1))
-		return;
-	if (e->xbutton.button == Button1)
+	if (e->xbutton.button == Button3)
+		selpaste(NULL);
+	else if (e->xbutton.button == Button1)
 		mousesel(e, 1);
 }
 
